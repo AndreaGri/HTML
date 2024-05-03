76dc12b6ddc3eb456d0c7f558b354dfa8a65a619
Andrea Grillo <grillo.andrea@iisgalvanimi.edu.it>
2024-05-03 10:00:12 +0000

    aggiornamento

--- meteo.py
+++ meteo.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def home():
   return render_template('homeMeteo.html', title='Meteo',sottotitolo=' Il meteo per tutti, disponibile 24h su 24h a tua disposizione ',inserimento='Inserisci qua il la città desiderata', cerca='Cerca')
 
+@app.route('/en')
+def homeen():
+  return render_template('homeMeteo.html', title='Weather forecast',sottotitolo='The weather for everyone, available 24 hours a day at your disposal',inserimento='Enter the desired city here', cerca='Search')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
--- static/js/Meteo.js
+++ static/js/Meteo.js
@@ -1,10 +1,28 @@
+
 function Cerca()  {
     let inp=document.getElementById('in')
     console.log(inp)
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+ inp.value +'&appid=d2013eafe2fed67ff84acda1b28014a7') 
     .then(response => response.json()) 
     .then(data => { console.log(data) 
-        var tempValue = data['main']['temp']; '''(32 °F - 32) × 5/9'''
-
-        console.log(tempValue) }) 
+        var tempValue = parseFloat(data['main']['temp'])-273.15
+        var lat=parseFloat(data['coord']['lat'])
+        var lon=parseFloat(data['coord']['lon'])
+        var tempmax=parseFloat(data['main']['temp_max'])-273.15
+        var tempmin=parseFloat(data['main']['temp_min'])-273.15
+        var tempperc=parseFloat(data['main']['feels_like'])-273.15
+        var meteo= String(data['weather']['0']['main'])
+        console.log(meteo)
+        console.log([tempmin,tempmax,tempperc])
+        console.log(tempValue)
+        console.log([lat,lon])
+        document.getElementById('meteo').innerHTML=(
+            'Temperatura: '+ tempValue+ '</br>'+
+            'Temperatura minima: ' + tempmin+ '</br>'+
+            'Temperatura massima: '+ tempmax + '</br>'+
+            'Temperatura percepita: '+ tempperc +'</br>'+
+            'Condizioni: '+meteo+'</br>'+
+            'Posizione: '+ [lat,lon]
+        ) 
+    }) 
 }
